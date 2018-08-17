22c33bb7770e0bf1eb3014a32906d4bb4cfc452d
eyhem18 <42380647+eyhem18@users.noreply.github.com>
2018-08-17 14:26:10 +0100

    Add files via upload

---
+++ G.M.C/js/main.js
@@ -0,0 +1,24 @@
+
+function bold(){
+  document.getElementById("txt").style.fontWeight="bold";
+}
+function undrlind(){
+  document.getElementById("txt").style.textDecoration="underline";
+}
+function ITALIC(){
+  document.getElementById("txt").style.fontStyle="ITALIC";
+}
+function thour()
+{
+  var soulahfat = document.getElementById("bagra").value; 
+ document.getElementById("txt").style.fontSize = soulahfat+ "px"
+}
+function boo()
+{
+  var katous = document.getElementById("hayawan").value; 
+ document.getElementById("txt").style.fontFamily =katous;
+}
+ 
+
+
+
